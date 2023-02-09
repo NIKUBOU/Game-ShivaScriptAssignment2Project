a83aae34117abe32912e1ccfe13430bba119acba
Shiva <patel_shiva23@hotmail.com>
2023-02-09 11:20:02 -0500

    Player can jump

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -5,15 +5,29 @@ using UnityEngine;
 public class PlayerMovementController : MonoBehaviour
 {
     private const string HORIZONTAL = "Horizontal";
+    private const string FIRE1 = "Fire1";
 
-    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 350f;
 
-    private void Update()
+    private new Rigidbody2D rigidbody2D;
+
+    private void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
     {
         float horizontal = Input.GetAxis(HORIZONTAL);
 
         Vector3 movement = new Vector3(horizontal, 0);
 
-        transform.position += movement * Time.deltaTime * moveSpeed;
+        transform.position += movement * Time.fixedDeltaTime * moveSpeed;
+
+        if (Input.GetButtonDown(FIRE1))
+        {
+            rigidbody2D.AddForce(Vector2.up * jumpForce);
+        }
     }
 }
