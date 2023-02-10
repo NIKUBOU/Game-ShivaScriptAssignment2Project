260d9782dd6938717973264eae6162c835453f20
Shiva <patel_shiva23@hotmail.com>
2023-02-09 19:00:57 -0500

    Made a Game Manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public int Lives { get; private set; }
+    public int Deaths { get; private set; }
+
+    public static GameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null)
+            Destroy(gameObject);
+        else
+        {
+            Lives = 3;
+            Deaths = 0;
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    internal void KillPlayer()
+    {
+        Lives--;
+        SceneManager.LoadScene(0);
+    }
+}
--- Assets/Scripts/KillOnTouch.cs
+++ Assets/Scripts/KillOnTouch.cs
@@ -1,15 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class KillOnTouch : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
         var player = collision.collider.GetComponent<PlayerMovementController>();
-        
+
         if (player != null)
-            SceneManager.LoadScene(0);
+            GameManager.Instance.KillPlayer();
     }
 }
