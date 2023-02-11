a39b76252c01f40291a57d58294019fd771ec032
Shiva <patel_shiva23@hotmail.com>
2023-02-11 18:14:34 -0500

    Moving Platform

--- Assets/Scripts/CharacterGrounding.cs
+++ Assets/Scripts/CharacterGrounding.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +10,9 @@ public class CharacterGrounding : MonoBehaviour
     [SerializeField] private float maxDistance;
     [SerializeField] private LayerMask layerMask;
 
+    private Transform groundedObject;
+    private Vector3? groundedObjectLastPosition; //Vector3? means that Vector3 can be null in this case
+
     public bool IsGrounded { get; private set; }
 
     private void Update()
@@ -17,14 +21,40 @@ public class CharacterGrounding : MonoBehaviour
 
         if (!IsGrounded)
             CheckSensorForGrounded(rightSensor);
+
+        StickToMovingObject();
+    }
+
+    private void StickToMovingObject()
+    {
+        if (groundedObject != null)
+        {
+            if (groundedObjectLastPosition.HasValue && groundedObjectLastPosition.Value != groundedObject.position)
+            {
+                Vector3 delta = groundedObject.position - groundedObjectLastPosition.Value;
+                transform.position += delta;
+            }
+            groundedObjectLastPosition = groundedObject.position;
+        }
+        else
+        {
+            groundedObjectLastPosition = null;
+        }
     }
 
     private void CheckSensorForGrounded(Transform sensor)
     {
         var raycastHit = Physics2D.Raycast(sensor.position, Vector2.down, maxDistance, layerMask);
+
         if (raycastHit.collider != null)
+        {
+            groundedObject = raycastHit.collider.transform;
             IsGrounded = true;
+        }
         else
+        {
+            groundedObject = null;
             IsGrounded = false;
+        }
     }
 }
