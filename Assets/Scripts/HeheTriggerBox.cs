6e81fd4d59a9a0b29c7c3c4a254bbb2d93dbac08
Shiva <patel_shiva23@hotmail.com>
2023-02-12 18:20:43 -0500

    minor bug fixed

--- Assets/Scripts/HeheTriggerBox.cs
+++ Assets/Scripts/HeheTriggerBox.cs
@@ -6,8 +6,20 @@ public class HeheTriggerBox : MonoBehaviour
 {
     [SerializeField] GameObject heheSaw;
 
+    private bool heheWorked;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        heheSaw.SetActive(true);
+        if (!heheWorked)
+        {
+            heheSaw.SetActive(true);
+            heheWorked = true;
+        }
+        else
+        {
+            heheSaw.SetActive(false);
+        }
     }
+
+
 }
